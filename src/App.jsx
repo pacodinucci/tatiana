45e47ad3dd46cc3f5f311c0d942709d205dd2b84
Francisco Di Nucci <franciscoldinucci@gmail.com>
2023-07-03 15:48:24 -0300

    error404

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/home' element={<Home/>} />
+        <Route path='/' element={<Home/>} />
         <Route path='/prensa' />
         <Route path='/libros' element={<Libro/>} />
       </Routes>
