e704c400a0cca97ad8affdd072140e6d413c5ed2
pacodinucci <franciscoldinucci@gmail.com>
2023-09-18 09:02:18 -0300

    animaciones fotos

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,7 +11,7 @@ import Footer from './Footer';
 
 
 function Home() {
-  
+
   const [elementoRef, isIntersecting] = useIntersection({
     threshold: 0.5,
   });
@@ -23,36 +23,36 @@ function Home() {
 
   return (
     <>
-      <NavBar/>
+      <NavBar />
       <section>
         <div className={styles.landing_container}>
           <div className={styles.menu_landing}>
-           
-           </div>
-          
-           
-            <div className={styles.name}>
-              <h1 translate='no'>TATIANA</h1>
-              <h1 translate='no'>GORANSKY</h1>
-            </div>
-          
+
+          </div>
+
+
+          <div className={styles.name}>
+            <h1 translate='no'>TATIANA</h1>
+            <h1 translate='no'>GORANSKY</h1>
+          </div>
+
         </div>
       </section>
       <section id="libros" className={styles.libros}>
-          <div className={styles.galeria_container}>
-            <div className={styles.ultimos}>
-              <h2>ÚLTIMOS LIBROS</h2>
-            </div>
-            <div className={styles.galeria}>
-              <a href="/libros/7"><img src={libro7} alt="" /></a>
-              <a href="/libros/6"><img src={libro6} alt="" /></a>
-              <a href="/libros/5"><img src={libro5} alt="" /></a>
-            </div>
-            <div className={styles.vermasbtn}>
-              <a href="/libros">VER TODOS</a>
-            </div>
+        <div className={styles.galeria_container}>
+          <div className={styles.ultimos}>
+            <h2>ÚLTIMOS LIBROS</h2>
           </div>
-          
+          <div className={styles.galeria}>
+            <a href="/libros/7"><img src={libro7} alt="" /></a>
+            <a href="/libros/6"><img src={libro6} alt="" /></a>
+            <a href="/libros/5"><img src={libro5} alt="" /></a>
+          </div>
+          <div className={styles.vermasbtn}>
+            <a href="/libros">VER TODOS</a>
+          </div>
+        </div>
+
       </section>
       <section id="biografia">
         <div className={styles.bio_container}>
@@ -61,29 +61,29 @@ function Home() {
           </div>
           <div className={styles.biografia_container}>
             <div className={styles.biografia_content}>
-             <img src={imagenBio} alt="" ref={elementoRef} className={`${styles["slide-in-right"]} ${isIntersecting ? styles.active : ''}`} />
-                {/* <h2>BIOGRAFÍA</h2> */}
-                <div className={`${styles.textosBio} ${styles["slide-in-left"]} ${isIntersecting ? styles.active : ''}`} ref={elementoRef2}>
-                  <p>Tatiana Goransky nació en Buenos Aires en 1977, es escritora, dramaturga y cantante <br /> de jazz. Autora de las novelas Lulúpe María T (2005), ¿Quién mató a la cantante de <br /> jazz? (2008), Don del agua (2010), Ball boy (2013), Fade Out (2016), Quisiera Amarte <br /> Menos (2020) y La mujer poco probable (2021).<br />
-                    En 2018 editó un número doble de la revista neoyorquina Los Bárbaros, que reúne a <br /> 43 autores de ocho países diferentes. En 2019, en Argentina y España, compiló la <br /> antología Barcelona / Buenos Aires, Once Mil kilómetros, un puente entre ambas <br /> ciudades.<br />
-                    Su trabajo ha sido publicado en Argentina, Chile, México, Bolivia, Ecuador, Alemania, <br /> Uruguay, España y los Estados Unidos.</p>
-                    <a href="/english"><img src={bandera} alt="" /></a> 
-                </div>
+              <img src={imagenBio} alt="" ref={elementoRef} className={`${styles["slide-in-right"]} ${isIntersecting ? styles.active : ''}`} />
+              {/* <h2>BIOGRAFÍA</h2> */}
+              <div ref={elementoRef2} className={`${styles.textosBio} ${styles["slide-in-left"]} ${isIntersecting ? styles.active : ''}`} >
+                <p>Tatiana Goransky nació en Buenos Aires en 1977, es escritora, dramaturga y cantante <br /> de jazz. Autora de las novelas Lulúpe María T (2005), ¿Quién mató a la cantante de <br /> jazz? (2008), Don del agua (2010), Ball boy (2013), Fade Out (2016), Quisiera Amarte <br /> Menos (2020) y La mujer poco probable (2021).<br />
+                  En 2018 editó un número doble de la revista neoyorquina Los Bárbaros, que reúne a <br /> 43 autores de ocho países diferentes. En 2019, en Argentina y España, compiló la <br /> antología Barcelona / Buenos Aires, Once Mil kilómetros, un puente entre ambas <br /> ciudades.<br />
+                  Su trabajo ha sido publicado en Argentina, Chile, México, Bolivia, Ecuador, Alemania, <br /> Uruguay, España y los Estados Unidos.</p>
+                <a href="/english"><img src={bandera} alt="" /></a>
+              </div>
             </div>
           </div>
         </div>
       </section>
       <section id="contacto" className={styles.contacto}>
         <div className={styles.contacto_container}>
-            <div className={styles.titulo_contacto}>
-              <h2>CONTACTO</h2>
-            </div>
-            <form action="" className={styles.form}>
-                <input className={styles.input} type="text" name="" id="" placeholder="Nombre"/>
-                <input className={styles.input} type="email" name="" id="" placeholder="Email"/>
-                <textarea className={styles.input} name="" id="" cols="30" rows="10" placeholder="Mensaje"></textarea>
-                <input className={styles.input} type="submit" value="Enviar"/>
-            </form>
+          <div className={styles.titulo_contacto}>
+            <h2>CONTACTO</h2>
+          </div>
+          <form action="" className={styles.form}>
+            <input className={styles.input} type="text" name="" id="" placeholder="Nombre" />
+            <input className={styles.input} type="email" name="" id="" placeholder="Email" />
+            <textarea className={styles.input} name="" id="" cols="30" rows="10" placeholder="Mensaje"></textarea>
+            <input className={styles.input} type="submit" value="Enviar" />
+          </form>
         </div>
       </section>
       <Footer />
--- src/components/Prensa.jsx
+++ src/components/Prensa.jsx
@@ -1,6 +1,6 @@
 import NavBar from "./NavBar";
 import Footer from "./Footer";
-import yt from '../assets/youtube.svg';
+import useIntersection from '../hooks/useIntersection';
 import styles from '../styles/Prensa.module.css';
 import galeria1 from '../assets/galeria1.png';
 import galeria2 from '../assets/galeria2.png';
@@ -10,67 +10,92 @@ import galeria5 from '../assets/galeria5.png';
 import galeria6 from '../assets/galeria6.png';
 
 function Prensa() {
-  return (
-    <>
-        <NavBar />
-        <div className={styles.content}>
-            <div className={styles.entrevista}>
-                <div className={styles.titulo}>
-                    <h2>ENTREVISTA</h2>
-                </div>
-                <div className={styles.video} style={{marginBottom: "-10vh", marginTop: "-10vh"}}>
-                    <iframe width="800" height="450" src="https://www.youtube.com/embed/ir0HcuiEyRo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen className={styles.reproductor}></iframe>
-                    {/* <a className={styles.btn}>
+
+    const [elementoRef, isIntersecting] = useIntersection({
+        threshold: 0.5,
+    });
+
+    const [elementoRef2, isIntersecting2] = useIntersection({
+        threshold: 0.5,
+    });
+
+    const [elementoRef3, isIntersecting3] = useIntersection({
+        threshold: 0.5,
+    });
+
+    const [elementoRef4, isIntersecting4] = useIntersection({
+        threshold: 0.5,
+    });
+
+    const [elementoRef5, isIntersecting5] = useIntersection({
+        threshold: 0.5,
+    });
+
+    const [elementoRef6, isIntersecting6] = useIntersection({
+        threshold: 0.5,
+    });
+
+    return (
+        <>
+            <NavBar />
+            <div className={styles.content}>
+                <div className={styles.entrevista}>
+                    <div className={styles.titulo}>
+                        <h2>ENTREVISTA</h2>
+                    </div>
+                    <div className={styles.video} style={{ marginBottom: "-10vh", marginTop: "-10vh" }}>
+                        <iframe width="800" height="450" src="https://www.youtube.com/embed/ir0HcuiEyRo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen className={styles.reproductor}></iframe>
+                        {/* <a className={styles.btn}>
                         <img src={yt} alt="" />
                         <h4>VER MÁS</h4>
                     </a> */}
+                    </div>
                 </div>
-            </div>
-            <div className={styles.entrevista}>
-                <div className={styles.titulo}>
-                    <h2>OTRAS ENTREVISTAS</h2>
-                </div>
-                <div style={{height: "40vh"}}>
+                <div className={styles.entrevista}>
+                    <div className={styles.titulo}>
+                        <h2>OTRAS ENTREVISTAS</h2>
+                    </div>
+                    <div style={{ height: "40vh" }}>
 
-                </div>
-            </div>
-            <div className={styles.fotosprensa}>
-                <div className={styles.titulo}>
-                    <h2>FOTOS DEL PAQUETE</h2>
-                    <h2>DE PRENSA</h2>
-                </div>
-                <div className={styles.galeria}>
-                    <div style={{marginBottom: "10vh"}}>
-                        <div className={styles.primera}>
-                            <div className={styles.primeraizq}>
-                                <img src={galeria1} alt="" />
-                            </div>
-                            <div className={styles.primeramedio}>
-                                <img src={galeria2} alt="" />
-                                <img src={galeria3} alt="" />
-                            </div>
-                            <div className={styles.primerader}>
-                                <img src={galeria4} alt="" />
-                            </div>
-                        </div>
-                        <h4>Fotos: Alejandro Meter</h4>
-                    </div>
-                    <div className={styles.segunda}>
-                        <div>
-                            <img src={galeria5} alt="" />
-                            <h4>Foto: Mailen Albamonte Pizarro</h4>
-                        </div>
-                        <div>
-                            <img src={galeria6} alt="" />
-                            <h4>Sin crédito</h4>
-                        </div>
                     </div>
                 </div>
+                <div className={styles.fotosprensa}>
+                    <div className={styles.titulo}>
+                        <h2>FOTOS DEL PAQUETE</h2>
+                        <h2>DE PRENSA</h2>
+                    </div>
+                    <div className={styles.galeria}>
+                        <div style={{ marginBottom: "10vh" }}  >
+                            <div className={styles.primera}>
+                                <div className={styles.primeraizq}>
+                                    <img src={galeria1} alt="" ref={elementoRef} className={`${styles["slide-in-right"]} ${isIntersecting ? styles.active : ''}`} />
+                                </div>
+                                <div className={styles.primeramedio}>
+                                    <img src={galeria2} alt="" ref={elementoRef3} className={`${styles["slide-in-top"]} ${isIntersecting3 ? styles.active : ''}`} />
+                                    <img src={galeria3} alt="" ref={elementoRef4} className={`${styles["slide-in-bottom"]} ${isIntersecting4 ? styles.active : ''}`} />
+                                </div>
+                                <div className={styles.primerader}>
+                                    <img src={galeria4} alt="" ref={elementoRef2} className={`${styles["slide-in-left"]} ${isIntersecting2 ? styles.active : ''}`} />
+                                </div>
+                            </div>
+                            <h4>Fotos: Alejandro Meter</h4>
+                        </div>
+                        <div className={styles.segunda}>
+                            <div>
+                                <img src={galeria5} alt="" ref={elementoRef5} className={`${styles["slide-in-right"]} ${isIntersecting5 ? styles.active : ''}`} />
+                                <h4>Foto: Mailen Albamonte Pizarro</h4>
+                            </div>
+                            <div>
+                                <img src={galeria6} alt="" ref={elementoRef6} className={`${styles["slide-in-left"]} ${isIntersecting6 ? styles.active : ''}`} />
+                                <h4>Sin crédito</h4>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
-        <Footer />
-    </>
-  )
+            <Footer />
+        </>
+    )
 }
 
 export default Prensa
