b30b2a10481c84c12b37578159881750ad8be5b2
pacodinucci <franciscoldinucci@gmail.com>
2023-09-18 10:26:30 -0300

    video fantasma

--- src/components/Fantasma.jsx
+++ src/components/Fantasma.jsx
@@ -22,7 +22,7 @@ function Fantasma() {
                     <p>La escritora y ghostwriter Tatiana Goransky responsponde a estas preguntas en la siguiente entrevista:</p>
                 </span>
                 <div className={styles.video} style={{marginTop: "-10vh", marginBottom: "10vh"}}>
-                    <iframe width="800" height="450" src="https://www.youtube.com/embed/ir0HcuiEyRo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen className={styles.reproductor}></iframe>
+                    <iframe width="800" height="450" src="https://www.youtube.com/embed/hE6FQKFE2kM?si=jkexdS6ZNfHLEWrU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen className={styles.reproductor}></iframe>
                     <a className={styles.btn} href="https://www.youtube.com/@tatianagoransky" target="_blank">
                         <img src={yt} alt="" />
                         <h4>VER MÁS</h4>
